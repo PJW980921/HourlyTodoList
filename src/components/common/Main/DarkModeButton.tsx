c29b224fa3c6ceeaffc2c5de769bc0dfa921203a
박지원 <jackgg12322@gmail.com>
2024-02-27 19:33:20 +0900

    refactor : 불필요한 선언 제거

--- src/components/common/Main/DarkModeButton.tsx
+++ src/components/common/Main/DarkModeButton.tsx
@@ -3,14 +3,11 @@ import { useTheme } from 'next-themes'
 import { BsMoonStarsFill } from "react-icons/bs";
 import { BsSunFill } from "react-icons/bs";
 import {  Switch } from 'antd';
-import type { InputRef } from 'antd';
 
 
 const DarkModeButton = () => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
-  const inputRef = useRef<InputRef>(null);
-  const [input, setInput] = useState(true);
 
   useEffect(() => {
     setMounted(true)
