a4168db2dd6f285d0e691d32c351c4f0b1fab59e
박지원 <jackgg12322@gmail.com>
2024-02-29 12:55:42 +0900

    feat : 드래그 앤 드롭 구현

--- src/components/common/Main/TodoMain.tsx
+++ src/components/common/Main/TodoMain.tsx
@@ -1,6 +1,7 @@
 import TodoForm from '../Footer/TodoForm';
 import { useEffect, useState } from 'react';
 import TodoItem from './TodoItem';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
 
 interface TodoProps {
 	id: string;
@@ -51,13 +52,48 @@ export default function TodoMain({ filter }: { filter: string }) {
 
 	const todoFiltered = todoStatusFilter();
 
+	const reorder = (list: TodoProps[], startIndex: number, endIndex: number) => {
+		const result = Array.from(list);
+		const [removed] = result.splice(startIndex, 1);
+		result.splice(endIndex, 0, removed);
+		saveTodosToLocalStorage(result);
+		return result;
+	};
+
+	const onDragEnd = (result: DropResult) => {
+		if (!result.destination) {
+			return;
+		}
+		setTodos((items) => {
+			if (!result.destination) {
+				return items;
+			}
+			return reorder(items, result.source.index, result.destination.index);
+		});
+	};
+
 	return (
 		<>
-			<main className='h-[25rem]  w-full overflow-scroll scroll-smooth rounded-[2rem] border-2 border-solid border-black-0 scrollbar-hide '>
-				{todoFiltered.map((item) => (
-					<TodoItem key={item.id} todo={item} onEdit={handleEdit} onDelete={handleDelete} />
-				))}
-			</main>
+			<DragDropContext onDragEnd={onDragEnd}>
+				<main className='h-[25rem] w-full overflow-scroll scroll-smooth rounded-[2rem] border-2 border-solid border-black-0 scrollbar-hide'>
+					<Droppable droppableId='droppable'>
+						{(provided) => (
+							<div ref={provided.innerRef} {...provided.droppableProps}>
+								{todoFiltered.map((item, index) => (
+									<Draggable key={item.id} draggableId={item.id} index={index}>
+										{(provided) => (
+											<div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+												<TodoItem todo={item} onEdit={handleEdit} onDelete={handleDelete} />
+											</div>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</div>
+						)}
+					</Droppable>
+				</main>
+			</DragDropContext>
 			<TodoForm onAdd={handleAdd} />
 		</>
 	);
